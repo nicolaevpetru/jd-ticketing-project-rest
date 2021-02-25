d2d0241ab1469bda65acc43d23280a925f6ddd4f
Petru <nicolaevpetru@outlook.com>
2021-02-24 21:15:35 -0500

    JD Ticketing Project Rest - logger added

--- src/main/java/com/ticketing/aspect/PerformanceAspect.java
+++ src/main/java/com/ticketing/aspect/PerformanceAspect.java
@@ -1,5 +1,6 @@
 package com.ticketing.aspect;
 
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -10,8 +11,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Aspect
 @Configuration
+@Slf4j
 public class PerformanceAspect {
-    Logger log = LoggerFactory.getLogger(PerformanceAspect.class);
+
 
     @Pointcut("@annotation(com.ticketing.annotation.ExecutionTime)")
     private void anyExecutionTimeOperation() {
